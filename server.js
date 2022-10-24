1a0d220b1934d064537823f022df371af6ba43bb
ALiasota <90059332+ALiasota@users.noreply.github.com>
2022-10-24 20:56:14 +0300

    255

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { DB_HOST } = process.env;
 const PORT = 3001;
 
 mongoose
-  .connect(DB_HOST)
+  .connect('mongodb+srv://valeri:8itBxyOWnNgSHM2o@cluster0.2665ccp.mongodb.net/superheroes?retryWrites=true&w=majority')
   .then(() =>
     app.listen(3001, () => {
       console.log(`Database connection successful on port ${PORT}`);
